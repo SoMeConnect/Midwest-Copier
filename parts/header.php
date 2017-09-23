3fec732298d37ae7c9ffa548775135b6f01dd108
lljb3 <lljb3@lljb3.com>
2017-09-23 13:52:59 -0500

    Added secondary nav

--- parts/header.php
+++ parts/header.php
@@ -5,3 +5,21 @@
     <style src="css/style.css" type="text/css" />
 </head>
 <body class="<?php echo getcwd() . "\n" ?>">
+
+<nav itemtype="http://schema.org/SiteNavigationElement" itemscope="itemscope" id="secondary-navigation">
+    <div class="inside-navigation grid-container grid-parent">
+        <button class="menu-toggle">
+            <span class="mobile-menu">Menu</span>
+        </button>
+        <div class="main-nav">
+            <ul id="menu-global-group" class=" secondary-menu">
+                <li id="menu-item-500" class="menu-item">
+                    <a href="https://arcoa.impactpreview.com/">ABOUT ARCOA GROUP</a>
+                </li>
+                <li id="menu-item-501" class="menu-item">
+                    <a href="https://arcoa.impactpreview.com/group/contact-us/">CONTACT US</a>
+                </li>
+            </ul>
+        </div>			
+    </div>
+</nav>
