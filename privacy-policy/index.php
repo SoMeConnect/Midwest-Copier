64ea0834318c118a0ac6fd8f3982a953cf639d8d
James Burrell <jamesburrell@SoMes-iMac.local>
2017-09-25 13:42:22 -0500

    Added privacy policy pages.

---
+++ privacy-policy/index.php
@@ -0,0 +1,167 @@
+<!-- HTML5 Header -->
+<?php require_once('../parts/header.php'); ?>
+
+<main class="exchange-template-default single single-exchange postid-287 wp-custom-logo post-image-above-header post-image-aligned-center secondary-nav-above-header secondary-nav-aligned-right slideout-enabled slideout-desktop sticky-menu-fade mega-menu-main-home-menu  featured-image-active left-sidebar nav-below-header fluid-header one-container nav-aligned-left header-aligned-left dropdown-click dropdown-click-menu-item">
+    <div class="hfeed site grid-container container grid-parent" id="page">
+        <div class="site-content" id="content">
+            <div class="content-area grid-parent mobile-grid-100 grid-100 tablet-grid-100" id="primary">
+                <main class="site-main" id="main">
+                    <article class="post-597 exchange type-exchange status-publish has-post-thumbnail hentry" id="post-597" itemscope="itemscope" itemtype="http://schema.org/CreativeWork">
+                        <div class="inside-article">
+                            <header class="entry-header">
+                                <h1 class="entry-title" itemprop="headline">Midwest Copier Exchange, LLC Web Portal Privacy Policy</h1>
+                            </header><!-- .entry-header -->
+                            <div class="entry-content" itemprop="text">
+                                <div class="privacy-policy-content">
+                                    <b>Midwest Copier Exchange, LLC. Privacy Policy</b><br>
+                                    <i>Date of Last Revision: October 19th, 2008</i>
+                                    <p><u>Introduction</u></p>
+                                    <p>At Midwest Copier Exchange, we recognize that privacy is important. This Privacy Policy
+                                    applies to all of the services and websites offered by Midwest Copier Exchange, LLC. or its
+                                    subsidiaries or affiliated companies unless explicitly stated otherwise.</p>
+                                    <p>If you have any questions about this Privacy Policy, please feel free to <a href=
+                                    "">contact us</a> through our website.</p>
+                                    <p><u>What Information Do We Collect and What For?</u></p>
+                                    <p>We offer a number of services that do not require you to register for an account or provide
+                                    any personal information to us, such as our Inventory Search. In order to provide our full range
+                                    of services, we may collect the following types of information:</p>
+                                    <ul>
+                                        <li>Information you provide - When you sign up for a Midwest Account, we ask you for personal
+                                        information (such as your name, email address and an account password which is maintained in
+                                        encrypted form on secure servers). We may combine the information you submit under your account
+                                        with information from other Midwest services or third parties in order to provide you with a
+                                        better experience and to improve the quality of our services. For certain services, we may give
+                                        you the opportunity to opt out of combining such information.</li>
+                                        <li>Cookies - When you visit Midwest, we send one or more cookies - a small file containing a
+                                        string of characters - to your computer that uniquely identifies your browser. We use cookies to
+                                        improve the quality of our service by storing user preferences and tracking user trends, such as
+                                        how/what people search for.</li>
+                                        <li>Log information - When you access Midwest services, our servers automatically record
+                                        information that your browser sends whenever you visit a website. These server logs may include
+                                        information such as your web request, Internet Protocol address, browser type, browser language,
+                                        the date and time of your request and one or more cookies that may uniquely identify your
+                                        browser.</li>
+                                        <li>User communications - When you send email or other communications to Midwest, we may retain
+                                        those communications in order to process your inquiries, respond to your requests and improve
+                                        our services.</li>
+                                        <li>Affiliated sites - We offer some of our services in connection with other web sites.
+                                        Personal information that you provide to those sites may be sent to Midwest in order to deliver
+                                        the service. We process such information in accordance with this Privacy Policy. The affiliated
+                                        sites may have different privacy practices and we encourage you to read their privacy
+                                        policies.</li>
+                                        <li>Other sites - This Privacy Policy applies to Midwest services only. We do not exercise
+                                        control over the sites linked from within our various services. These other sites may place
+                                        their own cookies or other files on your computer, collect data or solicit personal information
+                                        from you.</li>
+                                    </ul>
+                                    <p>Midwest only processes personal information for the purposes described in this Privacy Policy
+                                    and/or the supplementary privacy notices for specific services. In addition to the above, such
+                                    purposes include:</p>
+                                    <ul>
+                                        <li>Providing our services to users, including the display of customized content and
+                                        advertising;</li>
+                                        <li>Auditing, research and analysis in order to maintain, protect and improve our services;</li>
+                                        <li>Ensuring the technical functioning of our network; and</li>
+                                        <li>Developing new services.</li>
+                                    </ul>
+                                    <p>You can find more information about how we process personal information by referring to the
+                                    supplementary privacy notices for particular services.</p>
+                                    <p><u>Choices for personal information</u></p>
+                                    <p>When you sign up for a particular service that requires registration, we ask you to provide
+                                    personal information. If we use this information in a manner different than the purpose for which
+                                    it was collected, then we will ask for your consent prior to such use.</p>
+                                    <p>If we propose to use personal information for any purposes other than those described in this
+                                    Privacy Policy and/or in the specific service privacy notices, we will offer you an effective way
+                                    to opt out of the use of personal information for those other purposes. We will not collect or
+                                    use sensitive information for purposes other than those described in this Privacy Policy and/or
+                                    in the supplementary service privacy notices, unless we have obtained your prior consent.</p>
+                                    <p>Most browsers are initially set up to accept cookies, but you can reset your browser to refuse
+                                    all cookies or to indicate when a cookie is being sent. However, some Midwest features and
+                                    services may not function properly if your cookies are disabled.</p>
+                                    <p>You can decline to submit personal information to any of our services, in which case Midwest
+                                    may not be able to provide those services to you.</p>
+                                    <p><u>Information sharing</u></p>
+                                    <p>Midwest only shares personal information with other companies or individuals outside of
+                                    Midwest in the following limited circumstances:</p>
+                                    <ul>
+                                        <li>We have your consent. We require opt-in consent for the sharing of any sensitive personal
+                                        information.</li>
+                                        <li>We have a good faith belief that access, use, preservation or disclosure of such information
+                                        is reasonably necessary to (a) satisfy any applicable law, regulation, legal process or
+                                        enforceable governmental request, (b) enforce applicable Terms of Service, including
+                                        investigation of potential violations thereof, (c) detect, prevent, or otherwise address fraud,
+                                        security or technical issues, or (d) protect against imminent harm to the rights, property or
+                                        safety of Midwest, its users or the public as required or permitted by law.</li>
+                                    </ul>
+                                    <p>If Midwest becomes involved in a merger, acquisition, or any form of sale of some or all of
+                                    its assets, we will provide notice before personal information is transferred and becomes subject
+                                    to a different privacy policy.</p>
+                                    <p>We may share with third parties certain pieces of aggregated, non-personal information, such
+                                    as the number of users who searched for a particular term, for example. Such information does not
+                                    identify you individually.</p>
+                                    <p>Please contact us using the methods below for any additional questions about the management or
+                                    use of personal data.</p>
+                                    <p><u>Information security</u></p>
+                                    <p>We take appropriate security measures to protect against unauthorized access to or
+                                    unauthorized alteration, disclosure or destruction of data. These include internal reviews of our
+                                    data collection, storage and processing practices and security measures, as well as physical
+                                    security measures to guard against unauthorized access to systems where we store personal
+                                    data.</p>
+                                    <p>We restrict access to personal information to Midwest employees, contractors and agents who
+                                    need to know that information in order to operate, develop or improve our services. These
+                                    individuals are bound by confidentiality obligations and may be subject to discipline, including
+                                    termination and criminal prosecution, if they fail to meet these obligations.</p>
+                                    <p><u>Data integrity</u></p>
+                                    <p>Midwest processes personal information only for the purposes for which it was collected and in
+                                    accordance with this Privacy Policy or any applicable service-specific privacy notice. We review
+                                    our data collection, storage and processing practices to ensure that we only collect, store and
+                                    process the personal information needed to provide or improve our services. We take reasonable
+                                    steps to ensure that the personal information we process is accurate, complete, and current, but
+                                    we depend on our users to update or correct their personal information whenever necessary.</p>
+                                    <p><u>Accessing and updating personal information</u></p>
+                                    <p>When you use Midwest services, we make good faith efforts to provide you with access to your
+                                    personal information and either to correct this data if it is inaccurate or to delete such data
+                                    at your request if it is not otherwise required to be retained by law or for legitimate business
+                                    purposes. We ask individual users to identify themselves and the information requested to be
+                                    accessed, corrected or removed before processing such requests, and we may decline to process
+                                    requests that are unreasonably repetitive or systematic, require disproportionate technical
+                                    effort, jeopardize the privacy of others, or would be extremely impractical (for instance,
+                                    requests concerning information residing on backup tapes), or for which access is not otherwise
+                                    required. In any case where we provide information access and correction, we perform this service
+                                    free of charge, except if doing so would require a disproportionate effort. Some of our services
+                                    have different procedures to access, correct or delete users' personal information. We provide
+                                    the details for these procedures in the specific privacy notices or FAQs for these services.</p>
+                                    <p><u>Enforcement</u></p>
+                                    <p>Midwest regularly reviews its compliance with this Privacy Policy. Please feel free to direct
+                                    any questions or concerns regarding this Privacy Policy or Midwest's treatment of personal
+                                    information by <a href="/Contact.aspx">contacting us</a> through this web site or by writing to
+                                    us at</p>
+                                    <p></p>
+                                    <p>Online Privacy<br>
+                                    c/o Midwest Copier Exchange, LLC.<br>
+                                    3300 Washington Street<br>
+                                    Waukegan, Illinois, 60085<br>
+                                    USA</p>
+                                    <p>When we receive formal written complaints at this address, it is Midwest's policy to contact
+                                    the complaining user regarding his or her concerns. We will cooperate with the appropriate
+                                    regulatory authorities, including local data protection authorities, to resolve any complaints
+                                    regarding the transfer of personal data that cannot be resolved between Midwest and an
+                                    individual.</p>
+                                    <p><u>Changes to this Privacy Policy</u></p>
+                                    <p>Please note that this Privacy Policy may change from time to time. We will not reduce your
+                                    rights under this Privacy Policy without your explicit consent, and we expect most such changes
+                                    will be minor. Regardless, we will post any Privacy Policy changes on this page and, if the
+                                    changes are significant, we will provide a more prominent notice (including, for certain
+                                    services, email notification of Privacy Policy changes)</p>
+                                </div>
+                            </div><!-- .entry-content -->
+                        </div><!-- .inside-article -->
+                    </article><!-- #post-## -->
+                </main><!-- #main -->
+            </div><!-- #primary -->
+        </div><!-- #content -->
+    </div>
+</main>
+
+<!-- HTML5 Footer -->
+<?php require_once('../parts/footer.php'); ?>
